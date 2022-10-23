cc9700ac29d87c89095a725f400dbf4adba5c036
Ya Liu <sarahl0216@gmail.com>
2022-10-23 14:35:23 -0700

    initialize relay tutorial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+// your-app-name/src/App.js
+import React from 'react';
 import './App.css';
+import fetchGraphQL from './fetchGraphQL';
+
+const { useState, useEffect } = React;
 
 function App() {
+  // We'll load the name of a repository, initially setting it to null
+  const [name, setName] = useState(null);
+
+  // When the component mounts we'll fetch a repository name
+  useEffect(() => {
+    let isMounted = true;
+    fetchGraphQL(`
+      query RepositoryNameQuery {
+        # feel free to change owner/name here
+        repository(owner: "facebook" name: "relay") {
+          name
+        }
+      }
+    `).then(response => {
+      // Avoid updating state if the component unmounted before the fetch completes
+      if (!isMounted) {
+        return;
+      }
+      const data = response.data;
+      setName(data.repository.name);
+    }).catch(error => {
+      console.error(error);
+    });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  // Render "Loading" until the query completes
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {name != null ? `Repository: ${name}` : "Loading"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
